d282feb4ed9135cc87a1dda1060c186cdd4e9a48
mylastresort <pmloik300@gmail.com>
2022-10-27 13:33:36 +0000

    finishing main function

--- include/definitions.h
+++ include/definitions.h
@@ -6,16 +6,21 @@
 /*   By: stamim <stamim@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 16:30:18 by stamim            #+#    #+#             */
-/*   Updated: 2022/10/27 12:12:28 by stamim           ###   ########.fr       */
+/*   Updated: 2022/10/27 13:19:16 by stamim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINITIONS_H
 # define DEFINITIONS_H
 
-# define W 1920
-# define H 1080
-# define T "miniRT"
+enum e_WIN {
+	WIDTH = 1920,
+	HEIGHT = 1080,
+};
+
+# define TITLE "miniRT"
 
 # define INVLD_ARG "Invalid arguments"
+# define INVLD_SCN "Invalid scene"
+# define INVLD_FILE "Invalid .rt file"
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: stamim <stamim@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:28:53 by stamim            #+#    #+#             */
-/*   Updated: 2022/10/27 12:55:37 by stamim           ###   ########.fr       */
+/*   Updated: 2022/10/27 13:28:27 by stamim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,46 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <gsl/gsl_errno.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <string.h>
 #include <unistd.h>
 
+static void	fill(void *scn, int file)
+{
+	(void)file;
+	scn = NULL;
+	(void)scn;
+}
+
+static bool	is_rt_file(const char *arg)
+{
+	const size_t	sze = strlen(arg);
+
+	return (sze >= 3
+		&& arg[sze - 1] == 't' && arg[sze - 2] == 'r' && arg[sze - 3] == '.');
+}
+
+static void	run(void *scn)
+{
+	scn = NULL;
+	(void)scn;
+}
+
 int	main(const int argc, const char **argv)
 {
-	int	scene;
+	char	scn;
+	int		file;
 
 	if (argc != 2)
 		GSL_ERROR(INVLD_ARG, GSL_FAILURE);
-	scene = open(argv[1], O_CLOEXEC, S_IRUSR);
-	if (scene == -1)
+	if (!is_rt_file(argv[1]))
+		GSL_ERROR(INVLD_FILE, GSL_FAILURE);
+	file = open(argv[1], O_CLOEXEC, S_IRUSR);
+	if (file == -1)
 		GSL_ERROR(strerror(errno), GSL_FAILURE);
-	if (close(scene) == -1)
+	fill(&scn, file);
+	run(&scn);
+	if (close(file) == -1)
 		GSL_ERROR(strerror(errno), GSL_FAILURE);
 }
