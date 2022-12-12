026ecd531cca5bca648dde4ff24b49cbbf964a73
mylastresort <pmloik300@gmail.com>
2022-12-12 09:03:58 +0100

    todos got me stressed

--- include/declarations.h
+++ include/declarations.h
@@ -6,7 +6,7 @@
 /*   By: stamim <stamim@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:12:53 by stamim            #+#    #+#             */
-/*   Updated: 2022/12/07 11:23:18 by stamim           ###   ########.fr       */
+/*   Updated: 2022/12/12 09:03:23 by stamim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define DECLARATIONS_H
 
 # include "types.h"
+
+// TODO(stamim): mimic static_assert and assert
 
 int		destroy(t_scene *scn);
 void	parse(int file, t_scene *scn);
